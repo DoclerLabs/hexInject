f5e9dddd81a446546f1006091e363275cc1f0e8a
Francis Bourre <francis_bourre@me.com>
2016-04-23 20:38:39 +0200

    fix for: you cannot use @:build inside a macro

--- src/hex/di/IInjectorContainer.hx
+++ src/hex/di/IInjectorContainer.hx
@@ -3,8 +3,10 @@ package hex.di;
 /**
  * @author Francis Bourre
  */
+#if !macro
 @:remove
 @:autoBuild( hex.di.annotation.AnnotationReader.readMetadata( "hex.di.IInjectorContainer" ) )
+#end
 interface IInjectorContainer
 {
 	//TODO Remove this interface implementation during compilation
