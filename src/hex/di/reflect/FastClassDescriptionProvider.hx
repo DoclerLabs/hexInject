42300255f58d3176cc9092f79d4420a1c8e0ec78
francis.bourre <francis_bourre@me.com>
2016-09-22 07:53:36 +0200

    roll back to be compatible with haxe 3.2.*

--- src/hex/di/reflect/FastClassDescriptionProvider.hx
+++ src/hex/di/reflect/FastClassDescriptionProvider.hx
@@ -9,15 +9,8 @@ import hex.error.NullPointerException;
  */
 class FastClassDescriptionProvider implements IClassDescriptionProvider
 {
-	private var _data : Map<String, ClassDescription>;
-	
     public function new()
 	{
-		var injectionDataClass = Type.resolveClass( "InjectionData" );
-		if ( injectionDataClass != null )
-		{
-			this._data = ( cast injectionDataClass ).data;
-		}
 	}
 
     inline public function getClassDescription( type : Class<Dynamic> ) : ClassDescription
@@ -29,6 +22,6 @@ class FastClassDescriptionProvider implements IClassDescriptionProvider
 		}
 		#end
 		
-		return this._data.get( Type.getClassName( type ) );
+		return Reflect.getProperty( type, "__INJECTION_DATA" );
     }
 }
