e877655bc361042fffd0736ce99bcf462e2a6841
St3veV <stepan.vyterna@gmail.com>
2017-09-01 09:32:04 +0200

    MappingDefinitionUtil has now more semantical API since its functionality relates to the MappingDefinition and not to injector

--- src/hex/di/util/MappingDefinitionUtil.hx
+++ src/hex/di/util/MappingDefinitionUtil.hx
@@ -10,42 +10,36 @@ class MappingDefinitionUtil
 {
 	/** @private */ function new() throw new hex.error.PrivateConstructorException();
 
-	public static function addDefinition( target : GetInjector, mappings : Array<MappingDefinition>, useMetadata : Bool = true ) : Void
+	public static function addToInjector<T>( mappings : Array<MappingDefinition>, target : IDependencyInjector, ?useMetadataOf : T ) : Void
 	{
-		if ( useMetadata ) 
+		if ( useMetadataOf != null ) 
 		{
-			var cls = Type.getClass( target );
+			var cls = Type.getClass( useMetadataOf );
 			mappings = hex.di.mapping.MappingChecker.filter( cls, mappings );
 		}
-						
 
 		for ( mapping in  mappings )
 		{
 			if ( mapping.toValue != null )
 			{
-				target.getInjector().mapClassNameToValue( mapping.fromType, mapping.toValue, mapping.withName );
+				target.mapClassNameToValue( mapping.fromType, mapping.toValue, mapping.withName );
 			}
 			else
 			{
 				if ( mapping.asSingleton )
 				{
-					target.getInjector().mapClassNameToSingleton( mapping.fromType, mapping.toClass, mapping.withName );
+					target.mapClassNameToSingleton( mapping.fromType, mapping.toClass, mapping.withName );
 				}
 				else
 				{
-					target.getInjector().mapClassNameToType( mapping.fromType, mapping.toClass, mapping.withName );
+					target.mapClassNameToType( mapping.fromType, mapping.toClass, mapping.withName );
 				}
 			}
 			
 			if ( mapping.injectInto ) 
 			{
-				target.getInjector().injectInto( mapping.toValue );
+				target.injectInto( mapping.toValue );
 			}
 		}
 	}
-}
-
-typedef GetInjector =
-{
-	function getInjector() : IDependencyInjector;
 }
